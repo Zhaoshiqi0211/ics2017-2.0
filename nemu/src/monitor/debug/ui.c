d1658c4db02d82e181b4fff3fb9441b4ff884df0
tracer-ics2017 <tracer@njuics.org>
2019-03-27 00:26:24 +0800

    >  compile 161730217 zhaoshiqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.144-3 (2019-02-02) i686 GNU/Linux  00:26:24 up  2:12,  2 users,  load average: 0.00, 0.00, 0.00 29852993af6a6501abde02ba641762a4f10925b5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -79,9 +79,9 @@ static int cmd_si(char *args) {
    char *arg = strtok(NULL," ");
    int n=0;
    sscanf(arg,"%d",&n);
-   for(int j=0;j<n;++j){
-   cpu_exec(1);
-   }
+   //for(int j=0;j<n;++j){
+   cpu_exec(n);
+  // }
    return 1;
 }
 void ui_mainloop(int is_batch_mode) {
