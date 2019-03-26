74097e5e531782974a4357e90a8617317407126f
tracer-ics2017 <tracer@njuics.org>
2019-03-26 23:52:58 +0800

    >  compile 161730217 zhaoshiqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.144-3 (2019-02-02) i686 GNU/Linux  23:52:58 up  1:39,  2 users,  load average: 0.06, 0.03, 0.01 c9467ec272a2d0c0c7255e9d3d27bd72f8129ed7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+static int cmd_si(char *args);
 static struct {
   char *name;
   char *description;
@@ -46,7 +46,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "step few steps",  cmd_si }
   /* TODO: Add more commands */
 
 };
@@ -75,7 +75,15 @@ static int cmd_help(char *args) {
   }
   return 0;
 }
-
+static int cmd_si(char *args) {
+   char *arg = strtok(NULL," ");
+   int n=0;
+   sscanf(arg,"%d",&n);
+   for(int j=0;j<n;++j){
+   cpu_exec(1);
+   }
+   return 1;
+}
 void ui_mainloop(int is_batch_mode) {
   if (is_batch_mode) {
     cmd_c(NULL);
